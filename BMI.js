5c382e38ddb846232dfaaa5a92952d7ee8d330d8
pankajbhatt150 <76560049+pankajbhatt150@users.noreply.github.com>
2021-05-17 08:01:18 +0530

    Add files via upload

---
+++ BMI.js
@@ -0,0 +1,30 @@
+
+const express = require("express");
+const bodyParser = require("body-parser")
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/bmi.html");
+});
+
+app.post("/", function (req, res) {
+
+    var weight = Number(req.body.num1);
+    var height = Number(req.body.num2);
+
+    var result = n1 / (n2 * n2);
+
+    res.send("The result is " + result);
+});
+
+app.listen(3000, function () {
+    console.log("Server has started on port 3000.");
+});
+
+
+
